2d8da3ff1db929a67a6c404d7ba224460d422cde
RattanakpanhaKong <krattanakpanha@gmail.com>
2025-01-17 11:23:07 +0700

    Tab Navigation

--- app/(tabs)/AddNew.tsx
+++ app/(tabs)/AddNew.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function HomeScreeen() {
+export default function AddNew() {
   return (
     <View>
-      <Text>HomeScreeen</Text>
+      <Text>AddNew</Text>
     </View>
   )
 }
